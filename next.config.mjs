ea7af07489d165c2a3ffc2b4fe96585a0ca6f09e
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-08 20:56:52 -0400

    Set up Next.js serverless application

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'public, s-maxage=600, stale-while-revalidate=1800' }
+        ]
+      }
+    ];
+  }
+};
+
+export default nextConfig;
